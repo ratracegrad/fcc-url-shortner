9bed143fc230872e3b72b2d4f26d7fbbc7eedd6c
Jennifer Bland <ratracegrad@gmail.com>
2016-04-10 16:26:32 -0400

    Added model to store data in database

--- models/urlModel.js
+++ models/urlModel.js
@@ -1,3 +1,19 @@
 /**
  * Created by jbland on 4/10/16.
  */
+var mongoose = require('mongoose');
+var urlSchema = mongoose.Schema({
+    url: {
+        type:       String,
+        required:   true,
+        unique:     true
+    },
+    shortner: {
+        type:       String,
+        unique:     true
+    }
+});
+
+var URL = mongoose.model('URL', urlSchema);
+
+module.exports = URL;
